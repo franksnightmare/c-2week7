38218328f40308191233a0efe553bd44d6324014
Taragohf <ajos.chungos@gmail.com>
2017-01-13 01:46:13 +0100

    Exercise 52

---
+++ a52/main.cc
@@ -0,0 +1,44 @@
+#include <thread>
+#include <iostream>
+#include <future>
+#include <utility>
+#include <iomanip>
+
+double lhs[4][5] = {{1, 2, 3, 4, 5},
+		    {1, 2, 3, 4, 5},
+		    {1, 2, 3, 4, 5},
+		    {1, 2, 3, 4, 5}};
+
+double rhsT[6][5] = {{1, 2, 3, 4, 5},
+		     {1, 2, 3, 4, 5},
+		     {1, 2, 3, 4, 5},
+		     {1, 2, 3, 4, 5},
+		     {1, 2, 3, 4, 5},
+		     {1, 2, 3, 4, 5}};
+
+void innerProduct(std::promise<double> &ref, int row, int col)
+{
+    double sum = 0;
+    for (int idx = 0; idx != 5; ++idx)
+	sum += lhs[row][idx] * rhsT[col][idx];
+
+    ref.set_value(sum);
+}
+
+int main()
+{
+    std::promise<double> result[4][6];
+
+    for (int row = 0; row != 4; ++row)
+    {
+	for (int col = 0; col != 6; ++col)
+	    std::thread(innerProduct, ref(result[row][col]), row, col).detach();
+    }
+
+    for (int row = 0; row != 4; ++row)
+    {
+	for (int col = 0; col != 6; ++col)
+	    std::cout << result[row][col].get_future().get() << std::setw(6);
+	std::cout << '\n';
+    }
+}
